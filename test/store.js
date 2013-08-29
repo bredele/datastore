6ae5b464ac3c69e045bdf71cedd67f8addc33c16
bredele <olivier@petrofeed.com>
2013-08-28 22:57:54 -0600

    add test environment

---
+++ test/store.js
@@ -0,0 +1,13 @@
+var Store = require('store');
+var assert = require('assert');
+
+
+describe('object like', function(){
+  it('should set the data', function(){
+  });
+});
+
+describe('array like', function(){
+  it('should set the data', function(){
+  });
+});
