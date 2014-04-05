8e422cd3b4f34a24b82db54639ad0e328010b964
bredele <olivier.wietrich@gmail.com>
2014-04-04 20:35:04 -0600

    update for component emitter

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var storage = null,
-		Emitter,
+		Emitter = require('component-emitter'),
 		clone,
 		each;
 try {
   storage = window.localStorage;
-  Emitter = require('emitter');
   clone = require('clone');
   each = require('each');
 } catch(_) {
-	Emitter = require('component-emitter');
 	clone = require('clone-bredele');
   each = require('each-bredele');
 }
