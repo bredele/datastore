cfc1f9cb05d6a7ef34e82369bb251b174f0de2a3
bredele <olivier.wietrich@gmail.com>
2016-10-22 16:26:48 -0600

    should compute data

--- test/memory.js
+++ test/memory.js
@@ -9,24 +9,37 @@ var store = require('..')
 test('should initialize data', assert => {
   assert.plan(1)
   var data = store({
-    name: 'olivier'
+    label: 'hello'
   })
 
-  assert.equal(data.get('name'), 'olivier')
+  assert.equal(data.get('label'), 'hello')
 })
+
 
 test('should set data', assert => {
   assert.plan(1)
   var data = store()
-  data.set('name', 'olivier')
-  assert.equal(data.get('name'), 'olivier')
+  data.set('label', 'hello')
+  assert.equal(data.get('label'), 'hello')
 })
 
 
 test('should curry setter', assert => {
   assert.plan(1)
   var data = store()
-  var name = data.set('name')
-  name('olivier')
-  assert.equal(data.get('name'), 'olivier')
+  var label = data.set('label')
+  label('hello')
+  assert.equal(data.get('label'), 'hello')
+})
+
+test('should compute a store property', assert => {
+  assert.plan(1)
+  var data = store({
+    label: 'hello'
+  })
+
+  data.set('welcome', () => {
+    return this.label + ' world!'
+  })
+  assert.equal(data.get('label'), 'hello worl!')
 })
