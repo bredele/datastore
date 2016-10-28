9c34ed319b5aeeb9cd8170c115cb625ca5666bc2
bredele <olivier.wietrich@gmail.com>
2016-10-28 11:16:50 -0600

    use adapter if defined

--- test/adapter.js
+++ test/adapter.js
@@ -13,12 +13,51 @@ test('should set data with adapter', assert => {
       set(target, key, value) {
         setTimeout(() => {
           target(key, value + ' world')
-        }, 500)
+        }, 100)
       }
     }
   })
 
   data.set('label', 'hello').then(function() {
     assert.equal(data.get('label'), 'hello world')
+  })
+})
+
+test('shoud pull data with adapter', assert => {
+  assert.plan(1)
+  var data = store({
+    label: 'hello'
+  }, function(data) {
+    return {
+      pull(target, key) {
+        setTimeout(() => {
+          target(key)
+        }, 100)
+      }
+    }
+  })
+
+  data.pull('label').then(function(value) {
+    assert.equal(value, 'hello')
+  })
+})
+
+
+test('shoud delete data with adapter', assert => {
+  assert.plan(1)
+  var data = store({
+    label: 'hello'
+  }, function(data) {
+    return {
+      del(target, key) {
+        setTimeout(() => {
+          target(key)
+        }, 100)
+      }
+    }
+  })
+
+  data.del('label').then(function() {
+    assert.equal(data.get('label'), undefined)
   })
 })
