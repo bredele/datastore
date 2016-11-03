00bbf1bc2605dcceb824b870b1c266b7f05d3cdd
bredele <olivier.wietrich@gmail.com>
2016-11-03 14:16:03 -0600

    should set a datastore

---
+++ test/extra.js
@@ -0,0 +1,22 @@
+/**
+ * Tests dependencies.
+ */
+
+var test = require('tape')
+var store = require('..')
+
+
+test('should set a datastore', assert => {
+  assert.plan(1)
+  var element = store()
+  var styles = store({
+    background: 'red'
+  })
+  element.set('styles', styles)
+  element.on('changed styles', function(val) {
+    assert.deepEqual(val, {
+      background: 'green'
+    })
+  })
+  styles.set('background', 'green')
+})
